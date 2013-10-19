050fab8ae4386b2147240284238bcbfeb4a0b291
codedokode <codedokode@gmail.com>
2013-10-19 04:42:04 +0200

    Added inline comment form

--- thread.php
+++ thread.php
@@ -17,131 +17,55 @@
 <div class="page-thread">
 
     <div class="b-threads-list">
-        <div class="b-thread">
-            <div class="b-post first-post">            
-                <div class="post-number">#36541</div>
-                
-                <p>Здравствуйте! Есть скрипт:</p>
-                
-                <pre><code>for ($i = 1; $i &lt; 60; $i++)
-{
-  sleep(1);
-}
-echo &quot;Работа скрипта закончена&quot;;</code></pre>
+        <div class="b-thread">        
+            <?php require __DIR__.'/thread-body.php' ?>
 
-                <p>Этот скрипт выполняется 60 секунд, и выдаёт ответ. В случае 
-                изменения <code>60</code> на <code>65</code> скрипт не останавливает свою работу и
-                 зависает (браузер продолжает показывать обработку скрипта).</p>
-                
-                <p>В <code>php.ini</code> прописано <code>max_execution_time = 180000</code>, также 
-                в скрипте прописывал <code>set_time_limit(120)</code> и 
-                <code>ini_set('max_execution_time', 120)</code>, даже в
-                 <code>.htaccess</code> прописывал…</p>
-
-                <div class="hide-text">
-                <p> <code>php_value max_execution_time 180000</code>.</p>
-
-                <p>В <code>phpinfo()</code> все мои изменения показывал.
-                У меня выделенный сервер. Ответ от поддержки:</p>
-
-                <blockquote>Выполнение скриптов зависит от настройки вашего программного обеспечения.</blockquote>
-                <p>На Денвере тот же скрипт обрабатывается до 500 секунд!
-                Подскажите, пожалуйста, как заставить скрипт обрабатываться максимально возможное 
-                количество времени на моём сервере?</p>
+            <form action="" class="b-in-thread-form b-big-form">
+                <div class="row row-header">
+                    <h2>Вам слово</h2>
+                    <p class="mb-0">Хотите добавить свой комментарий? 
+                    Пожалуйста, воспользуйтесь этой формой.</p>
                 </div>
-            </div>
 
-            <div class="b-post">            
-                <div class="post-number">#36545</div>
-                
-                <p>Иди мануал почитай что ли</p>        
-            </div>
+                <div class="row">
+                    <div class="row-comment">Ведите себя воспитанно. Невоспитанных ловят
+                    наши коты-модераторы.</div>
+                    <label class="row-label" for="add-text">Комментарий:</label>
+                    <textarea id="add-text" class="textarea-wide textarea-medium">Код надо писать не как 
+попало, а аккуратно и красиво. Почему? Потому, что на неакуратно написанный 
+код не хочется даже смотреть.
 
-            <div class="b-post">            
-                <div class="post-number">#36545</div>
-                
-                <p>Держи кота. Удачи тебе.</p>        
-            </div>
+Если тебе лень выравнивать код руками, закачай его на http://beta.phpformatter.com/ и
+нажми «format». Робот исправит выравнивание и отступы в мгновение ока. 
 
-            <div class="b-post">            
-                <div class="post-number">#36545</div>
-                
-                <p>PHP не нужен.</p>        
-            </div>
+Самый распространенный стандарт оформления — это Zend Coding Guides 
+(http://framework.zend.com/manual/1.12/en/coding-standard.html — на англ. яз.),
+вот их суть:
 
-            <div class="b-post">            
-                <div class="post-number">#36545</div>
-                
-                <p>Код надо писать не как попало, а аккуратно и красиво. Почему? Потому, что на неакуратно написанный код не хочется даже смотреть.</p>
-
-                <p>Если тебе лень выравнивать код руками, закачай его на http://beta.phpformatter.com/ 
-                и нажми «format». Робот исправит выравнивание и отступы в мгновение ока. <p>
-
-                <p>Самый распространенный стандарт оформления — это Zend Coding Guides 
-                (http://framework.zend.com/manual/1.12/en/coding-standard.html — на англ. яз.), 
-                вот их суть:</p>
-
-                <ul><li> переменные и функции пишутся с маленькой буквы, _ не используется, используется camelCase, пример: $x, $numberOfPeople, printResults()
-                <li>Название функции начинается с глагола, в стиле «сделайЧтоТо»
-                <li>не знаешь английский? Не беда, в 21 веке есть решение этой проблемы. Не пиши транслитом, открой лучше Гугл Транслейт или slovari.yandex.ru и найди название для переменной там
-                <li>в именах классов используется CamelCase, первая буква большая, «_» может
-                 использоваться
-- мы предпочитаем подстановку переменных вместо конкатенации строк:
- "I am $age years old" — хорошо, 'I am ' . $age . ' years old' — плохо       
-                </ul>
-            </div>
-
-            <div class="b-post">            
-                <div class="post-number">#36545</div>
-                
-                 <p>Код надо писать не как попало, а аккуратно и красиво. Почему? Потому, что на неакуратно написанный код не хочется даже смотреть.</p>
-
-                <p>Если тебе лень выравнивать код руками, закачай его на http://beta.phpformatter.com/ 
-                и нажми «format». Робот исправит выравнивание и отступы в мгновение ока. <p>    
-            </div>
-
-            <div class="b-post">            
-                <div class="post-number">#36545</div>
-                
-                <p>Иди мануал почитай что ли</p>        
-            </div>
-
-            <div class="b-post">            
-                <div class="post-number">#36545</div>
-                
-                <p>Держи кота. Удачи тебе.</p>        
-            </div>
-
-            <div class="b-post">            
-                <div class="post-number">#36545</div>
-                
-                <p>Умри в муках. Удачи тебе.</p>        
-            </div>
-
-            <div class="b-post">            
-                <div class="post-number">#36545</div>
-                
-                <p>Код сам себя не напишет нужно написать код.</p>        
-            </div>
-
-            <div class="b-post">            
-                <div class="post-number">#36545</div>
-                
-                <p>Умри в муках, неудачник</p>        
-            </div>
-
-            <div class="b-post last-post">            
-                <div class="post-number">#36545</div>
-                
-                <p>Я ненавижу весь мир и тебя лично</p>        
-            </div>
-
-            <div class="b-thread-actions clearfix">
-                <div class="aligned-right">                    
-                    <a href="board.php" class="button button-left back-link">← вернуться на главную</a>
-                    <a href="" class="button button-action button-reply">Добавить комментарий</a>
+- переменные и функции пишутся с маленькой буквы, _ не используется, 
+используется camelCase, пример: $x, $numberOfPeople, printResults()
+- Название функции начинается с глагола, в стиле «сделайЧтоТо»
+- не знаешь английский? Не беда, в 21 веке есть решение этой проблемы. Не пиши транслитом,
+открой лучше Гугл Транслейт или slovari.yandex.ru и найди название для переменной там
+- в именах классов используется CamelCase, первая буква большая, «_» может использоваться
+- мы предпочитаем подстановку переменных вместо конкатенации строк: "I am $age years old" —
+хорошо, 'I am ' . $age . ' years old' — плохо 
+- мы используем для отступов 4 пробела (можно настроить редактор, чтобы при нажатии Tab
+он вставлял 4 пробела)
+- скобки в for и if/else ставятся так:</textarea>
                 </div>
-            </div>
+
+                <div class="row">            
+                    <label class="row-label" for="add-name">Ваше имя:</label>
+                    <input type="text" id="add-name" class="input-wide">
+                    <div class="row-hint">не обязательно</div>
+               </div>
+
+               <div class="row row-buttons">
+                    <button class="button-action button-main">Добавить комментарий</button>
+                    <a href="board.php" class="button-left back-link">← вернуться на главную</a>
+               </div>
+           </form>            
         </div>
     </div>
 </div>
